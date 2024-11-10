02dc049e0ca1ea3c8a9d99b44243feebf45513e2
taisei08 <829taisei829@gmail.com>
2024-11-10 18:25:29 +0900

    fix: POSTができないようになっていたのでmodelを変更して修正

--- infra/mysqldb/album.go
+++ infra/mysqldb/album.go
@@ -70,7 +70,7 @@ func (r *albumRepository) Get(ctx context.Context, id model.AlbumID) (*model.Alb
 
 func (r *albumRepository) Add(ctx context.Context, album *model.Album) error {
 	query := "INSERT INTO albums (id, title, singer_id) VALUES (?, ?, ?)"
-	if _, err := r.db.ExecContext(ctx, query, album.ID, album.Title, album.Singer.ID); err != nil {
+	if _, err := r.db.ExecContext(ctx, query, album.ID, album.Title, album.SingerID); err != nil {
 		return err
 	}
 	return nil
--- model/album.go
+++ model/album.go
@@ -5,6 +5,7 @@ type AlbumID int
 type Album struct {
 	ID       AlbumID  `json:"id,omitempty"`
 	Title    string   `json:"title,omitempty"`
+	SingerID SingerID `json:"singer_id,omitempty"` // リクエストの形式を変えてこのフィールドを削除したい
 	Singer   Singer   `json:"singer,omitempty"`
 }
 
